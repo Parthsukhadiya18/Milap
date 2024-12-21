fd6859413f5b4401ccbac35b53d20204a0e3353f
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2024-12-21 12:48:09 +0530

    Add files via upload

---
+++ js/04_variable.js
@@ -0,0 +1,31 @@
+// var a;
+// a = 10;
+// console.log("a", a);
+// var a = 20;
+// console.log("a", a);
+
+// a=30;
+// console.log("a", a);
+
+
+// let text = "parth";
+// console.log("name", text);
+// SyntaxError: Identifier 'text' has already been declared 
+// let text = "parth";
+// console.log("name", text);
+// text = "earth";
+// console.log("name", text);
+
+const pi = 3.14;
+console.log("pi", pi);
+// SyntaxError: Identifier 'pi' has already been declared
+// const pi=3.14;
+// console.log("pi",pi);
+// TypeError: Assignment to constant variable.
+pi = 3.14;
+console.log("pi", pi);
+
+
+
+
+
---
+++ js/05_variablevalue.js
@@ -0,0 +1,52 @@
+// premetive data type 
+// // number 
+// let a = 10;
+// console.log("num", a);
+// string 
+// let text="parth patel";
+// console.log("name",text);
+// bool
+// let on =true;
+// console.log("bool",on);
+// empty
+// let empty="";
+// console.log("empty",empty)
+// undfin 
+// let undfin;
+// console.log("undfin",undfin)
+
+// non premetive data type 
+// array 
+// let number=[1,2,3,4,5]
+
+// console.log("array",number);
+// let Fname=["milap","sujal","Tushar"]
+
+// console.log("arrayFname",Fname);
+
+// let nameall=["milap"," ",1,true]
+
+// console.log("nameall",nameall);
+
+// objcete key:value 
+// let Address = { no: 123, Society: "Anmoal", pincode: 123456 }
+
+// console.log("obj", Address);
+// console.log("obj", Address.no);
+// console.log("obj", Address.pincode);
+
+let Address1 = {
+    no: 123, Society: "Anmoal",
+    pincode: {
+        pincod1: 12345,
+        pincod2: 82345
+    }
+}
+
+console.log("obj", Address1);
+console.log("obj", Address1.pincode);
+console.log("obj", Address1.pincode.pincod1);
+
+
+
+
