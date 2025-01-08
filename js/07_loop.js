13f85a6c09f526eca7c1a395bab303bc59b32d64
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2025-01-08 09:52:55 +0530

    Add files via upload

---
+++ js/07_loop.js
@@ -0,0 +1,83 @@
+// loop? 
+// While loop
+
+// var a = 0;
+// while (a < 5) {
+//   console.log("Hello");
+//   a++;
+// }
+
+
+// Do while loop
+
+// do {
+//   console.log("Hello");
+//   a++;
+// } while (a < 5)
+
+
+// for loop
+
+// for (a = 0; a < 5; a++) {
+
+//   console.log("Hello");
+
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// let text = ""
+// for (var a = 1; a <= 5; a++) {
+
+//   for (var b = 1; b <= a; b++) {
+//     text += "*"
+
+
+// console.log("*")
+// }
+//   text += '\n'
+//   // console.log("\n")
+// }
+// console.log(text)
+
+
+// let arry = [1, 2, 3, 4]
+
+// text = ""
+// for (a = 0; a < arry.length; a++) {
+
+//   let arry = [1, 2, 3, 4]
+//   text += arry[a];
+
+// } console.log(text);
+
+// let obj = { name: "parth", num: 1233 };
+// text = ""
+// for (const x in obj) {
+
+//   text += obj[x] + " ";
+
+
+// }
+// console.log(text);
+// text = ""
+
+// let obj = [1, 2, 3, 4]
+
+// for (const x of obj) {
+//   text += x + " "
+
+// } console.log(text);
+
+let obj = [1, 2, 3, 4]
+text = ""
+obj.forEach(element => {
+  text = obj + " "
+});
+console.log(text);
+
+// document.write("hi")
+
